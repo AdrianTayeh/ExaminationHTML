f63f7e61e613cdb8ba0c86c140a24a78cacccc51
adriantayeh <adriantayeh@gmail.com>
2024-10-09 16:04:25 +0200

    changed swiper to load from cdn

--- dist/index.e82f28a0.js
+++ dist/index.e82f28a0.js
@@ -588,49 +588,6 @@ var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 var _swiper = require("swiper");
 var _swiperDefault = parcelHelpers.interopDefault(_swiper);
 var _swiperBundleCss = require("swiper/swiper-bundle.css");
-const galleryImages = document.querySelectorAll(".imgContainer");
-const fullscreenSwiper = document.getElementById("fullscreenSwiper");
-let swiperInstance = null;
-console.log(galleryImages);
-function initSwiper(initialSlide) {
-    if (!swiperInstance) swiperInstance = new (0, _swiperDefault.default)(".fullscreenSwiper", {
-        initialSlide: initialSlide,
-        spaceBetween: 10,
-        slidesPerView: 1,
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev"
-        },
-        pagination: {
-            el: ".swiper-pagination"
-        }
-    });
-    else swiperInstance.slideTo(initialSlide, 0);
-    fullscreenSwiper.style.display = "block";
-    document.querySelector(".swiper-button-next").addEventListener("click", ()=>{
-        swiperInstance.slideNext();
-    });
-    document.querySelector(".swiper-button-prev").addEventListener("click", ()=>{
-        swiperInstance.slidePrev();
-    });
-}
-fullscreenSwiper.addEventListener("click", function(e) {
-    if (e.target == fullscreenSwiper) fullscreenSwiper.style.display = "none";
-});
-galleryImages.forEach((img, index)=>{
-    img.addEventListener("click", ()=>{
-        initSwiper(index);
-    });
-});
-document.querySelector(".close-swiper").addEventListener("click", ()=>{
-    fullscreenSwiper.style.display = "none";
-    swiperInstance.destroy();
-    swiperInstance = null;
-});
-function myFunction() {
-    var navLinks = document.getElementById("myLink");
-    navLinks.classList.toggle("show");
-}
 
 },{"swiper":"iM6UL","swiper/swiper-bundle.css":"aErfw","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"iM6UL":[function(require,module,exports) {
 /**
